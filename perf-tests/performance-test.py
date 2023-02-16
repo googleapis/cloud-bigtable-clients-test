1451641d1a4df49fcfef16ce0f0073cf323644af
Daniel Sanche <d.sanche14@gmail.com>
2023-02-16 12:17:06 -0800

    cleaned up server code

--- perf-tests/performance-test.py
+++ perf-tests/performance-test.py
@@ -8,25 +8,28 @@ import test_proxy_pb2_grpc as proxy_grpc
 import grpc.experimental
 import concurrent.futures as futures
 
-def create_client(client_id, mock_server_addr="localhost:8081", server_addr="localhost:9999", project_id="project", instance_id="instance"):
-    # channel = grpc.secure_channel(server_addr, grpc.ssl_channel_credentials())
+def create_client(client_id, server_addr, proxy_addr, project_id="project", instance_id="instance"):
+    """
+    server_addr: the address of the mock server running in this process
+    proxy_addr: the address of the client library proxy running in a different process
+    """
 
     proxy_client = proxy_grpc.CloudBigtableV2TestProxy()
     request = test_proxy_pb2.CreateClientRequest(
             client_id=client_id,
-            data_target=mock_server_addr,
+            data_target=server_addr,
             project_id=project_id,
             instance_id=instance_id,
     )
-    response = proxy_client.CreateClient(request, server_addr, insecure=True)
+    response = proxy_client.CreateClient(request, proxy_addr, insecure=True)
     return response
 
-def read_rows(server_addr="localhost:9999"):
+def read_rows(client_id, proxy_addr):
     tablename = f"projects/project/instances/instance/tables/table"
     bt_request = bigtable_pb2.ReadRowsRequest(table_name=tablename, rows_limit=5)
-    request = test_proxy_pb2.ReadRowsRequest(client_id="test", request=bt_request)
+    request = test_proxy_pb2.ReadRowsRequest(client_id=client_id, request=bt_request)
     proxy_client = proxy_grpc.CloudBigtableV2TestProxy()
-    response = proxy_client.ReadRows(request, server_addr, insecure=True)
+    response = proxy_client.ReadRows(request, proxy_addr, insecure=True)
     return response
 
 class MockBigtableServicer(bigtable_pb2_grpc.BigtableServicer):
@@ -35,19 +38,24 @@ class MockBigtableServicer(bigtable_pb2_grpc.BigtableServicer):
             yield bigtable_pb2.ReadRowsResponse(chunks=[])
 
 
-def serve():
+def serve(server_addr="localhost:8081"):
   print("starting server")
   server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
   bigtable_pb2_grpc.add_BigtableServicer_to_server(
        MockBigtableServicer(), server)
-  server.add_insecure_port('[::]:8081')
+  server.add_insecure_port(server_addr)
   server.start()
   return server
 
 if __name__ == "__main__":
-    server = serve()
-    c = create_client("test")
+    proxy_addr = "localhost:9999"
+    server_addr = "localhost:8888"
+
+    client_id = "test_client"
+
+    server = serve(server_addr=server_addr)
+    c = create_client(client_id, server_addr=server_addr, proxy_addr=proxy_addr)
     print(f"c = {c}")
-    r = read_rows()
+    r = read_rows(client_id, proxy_addr)
     print(f"r = {r}")
-    server.wait_for_termination()
+    # server.wait_for_termination()
