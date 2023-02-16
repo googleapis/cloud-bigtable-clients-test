cb1d1d9d8b6a3085c06e8532188817f3346f3a24
Daniel Sanche <d.sanche14@gmail.com>
2023-02-15 17:16:21 -0800

    got test to run against java client

--- perf-tests/performance-test.py
+++ perf-tests/performance-test.py
@@ -1,3 +1,25 @@
 import data_pb2
 import bigtable_pb2
 import test_proxy_pb2
+import test_proxy_pb2_grpc as proxy_grpc
+import grpc
+
+def create_client(client_id, server_addr="localhost:9999", project_id="project", instance_id="instance"):
+    channel = grpc.secure_channel(server_addr, grpc.ssl_channel_credentials())
+
+    proxy_client = proxy_grpc.CloudBigtableV2TestProxy()
+    request = test_proxy_pb2.CreateClientRequest(
+            client_id="test",
+            data_target=server_addr,
+            project_id=project_id,
+            instance_id=instance_id,
+    )
+    response = proxy_client.CreateClient(request, server_addr, insecure=True)
+    return response
+
+def read_row():
+    pass
+
+if __name__ == "__main__":
+    c = create_client("sanche")
+    print(c)
