f74b0b0b0308aa4d695f495e4c82af7d88f365fc
Daniel Sanche <d.sanche14@gmail.com>
2023-03-02 16:45:15 -0800

    got client set up and tear down working

--- perf-tests/proxy_server.py
+++ perf-tests/proxy_server.py
@@ -29,52 +29,54 @@ def proxy_server_process(queue_map):
     import test_proxy_pb2_grpc
 
     # from proxy_server_2 import CreateClient as proxy_client
-    def defer_to_client(func, timeout_seconds=10):
-        def wrapper(obj, request, context, **kwargs):
-            deadline = time.time() + timeout_seconds
-            json_dict = json_format.MessageToDict(request)
-            json_dict["proxy_request"] = func.__name__
-            (in_q, out_q) = queue_map[func.__name__]
-            in_q.put(json_dict)
-            # wait for response
-            while time.time() < deadline:
-                if not out_q.empty():
-                    response = out_q.get()
-                    print(f"read rows response: {response=}")
-                    return func(obj, request, context, **kwargs, response=response)
-                else:
-                    time.sleep(0.1)
-        return wrapper
+
 
 
     class TestProxyServer(test_proxy_pb2_grpc.CloudBigtableV2TestProxyServicer):
 
-        def CreateClient(self, request, context):
+        def defer_to_client(func, timeout_seconds=10):
+            def wrapper(obj, request, context, **kwargs):
+                deadline = time.time() + timeout_seconds
+                json_dict = json_format.MessageToDict(request)
+                json_dict["proxy_request"] = func.__name__
+                (in_q, out_q) = queue_map[func.__name__]
+                in_q.put(json_dict)
+                # wait for response
+                while time.time() < deadline:
+                    if not out_q.empty():
+                        response = out_q.get()
+                        print(f"{func.__name__} client response: {response=}")
+                        return func(obj, request, context, **kwargs, response=response)
+                    else:
+                        time.sleep(0.1)
+            return wrapper
+
+        @defer_to_client
+        def CreateClient(self, request, context, response=None):
             return test_proxy_pb2.CreateClientResponse()
 
-
+        @defer_to_client
         def CloseClient(self, request, context):
-            print("close the client")
-            print(f"{request=}")
             return test_proxy_pb2.CloseClientResponse()
+
+        @defer_to_client
+        def RemoveClient(self, request, context, response=None):
+            print(request)
+            return test_proxy_pb2.RemoveClientResponse()
+
+        @defer_to_client
+        def ReadRows(self, request, context, response=None):
+            return test_proxy_pb2.RowsResult()
 
         def ReadRow(self, request, context):
             print(f"readrow: {request=}")
             return test_proxy_pb2.RowResult()
 
-        @defer_to_client
-        def ReadRows(self, request, context, response=None):
-            print(f"read rows response: {response=}")
-            return test_proxy_pb2.RowsResult()
-
         def MutateRow(self, request, context):
             print(f"mutate rows: {request.client_id=} {request.request=}" )
             return test_proxy_pb2.MutateRowResult()
 
-        def RemoveClient(self, request, context):
-            print(f"removeclient request {request.client_id=}")
-            print(request)
-            return test_proxy_pb2.RemoveClientResponse()
+
 
     port = '50055'
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
@@ -87,28 +89,52 @@ def proxy_server_process(queue_map):
 def client_process(queue_map):
     import google.cloud.bigtable_v2 as bigtable_v2
     print("listening")
-    client = bigtable_v2.BigtableClient()
 
     class TestProxyHandler():
+        def __init__(self):
+            self.closed = False
+            self.client = bigtable_v2.BigtableClient()
+
+        def close(self):
+            self.closed = True
 
         def ReadRows(self, request_dict):
+            if self.closed:
+                raise RuntimeError("client is closed")
             return {"status": "ok"}
 
-    handler = TestProxyHandler()
     in_queues = [queue.in_q for queue in queue_map.values()]
+    client_map = {}
     while True:
         for q in in_queues:
             if not q.empty():
                 json_data = q.get()
                 # print(json_data)
-                fn = getattr(handler, json_data["proxy_request"])
-                result = fn(json_data)
-                (_, out_q) = queue_map[json_data["proxy_request"]]
-                out_q.put(result)
+                fn_name = json_data.pop("proxy_request")
+                (_, out_q) = queue_map[fn_name]
+                client_id = json_data.get("clientId", None)
+                client = client_map.get(client_id, None)
+                if fn_name == "CreateClient":
+                    client = TestProxyHandler()
+                    client_map[client_id] = client
+                    out_q.put(True)
+                elif client is None:
+                    raise RuntimeError("client not found")
+                elif fn_name == "CloseClient":
+                    client.close()
+                    out_q.put(True)
+                elif fn_name == "RemoveClient":
+                    client_map.pop(json_data["client_id"], None)
+                    out_q.put(True)
+                else:
+                    # run actual rpc against client
+                    fn = getattr(client, fn_name)
+                    result = fn(json_data)
+                    out_q.put(result)
         time.sleep(0.1)
 
 if __name__ == '__main__':
-    rpc_names = ["ReadRows"]
+    rpc_names = ["ReadRows", "CreateClient", "CloseClient", "RemoveClient"]
     RpcQueues = namedtuple("RpcQueues", ["in_q", "out_q"])
     queue_map = {name: RpcQueues(multiprocessing.Queue(), multiprocessing.Queue()) for name in rpc_names}
     # proxy_server_process(queue_map)
