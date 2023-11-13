49a22c9e66391f7817e2e41f9279989ae08fdc40
Jiongxin Liu <liujiongxin@gmail.com>
2023-11-13 10:29:09 -0500

    Add a feature_gap test that is only run when all the client features are supposed to be enabled.

---
+++ tests/feature_gap_test.go
@@ -0,0 +1,89 @@
+// Copyright 2023 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package tests
+
+import (
+	"encoding/base64"
+	"testing"
+
+	"github.com/golang/protobuf/proto"
+	"github.com/googleapis/cloud-bigtable-clients-test/testproxypb"
+	"github.com/stretchr/testify/assert"
+	btpb "google.golang.org/genproto/googleapis/bigtable/v2"
+	"google.golang.org/grpc/metadata"
+	"google.golang.org/protobuf/reflect/protoreflect"
+)
+
+// TestFeatureGap tests that all the optional features of Cloud Bigtable clients are truly enabled.
+func TestFeatureGap(t *testing.T) {
+	if !*enableFeaturesAll {
+		t.Logf("Skip the check as --enable_features_all is false")
+		return
+	}
+
+	tableName := buildTableName("table")
+
+	// 1. Instantiate the mock server.
+	headers := make(chan metadata.MD, 1)
+	server := initMockServer(t)
+	server.ReadRowsFn = func(req *btpb.ReadRowsRequest, srv btpb.Bigtable_ReadRowsServer) error {
+		md, _ := metadata.FromIncomingContext(srv.Context())
+		headers <- md
+		return nil
+	}
+
+	// 2. Build the request to test proxy
+	req := testproxypb.ReadRowRequest{
+		ClientId:  t.Name(),
+		TableName: tableName,
+		RowKey:    "row-01",
+	}
+
+	// 3. Perform the operation via test proxy
+	doReadRowOp(t, server, &req, nil)
+
+	// 4. Extract, decode, and parse the "bigtable-features" field in the header
+	header := <-headers
+	if len(header["bigtable-features"]) == 0 {
+		t.Fatalf("bigtable-features is missing in the request header")
+	}
+	featureEncoding := header["bigtable-features"][0]
+	t.Logf("Encoded feature flags: %s", featureEncoding)
+
+	decode, err := base64.URLEncoding.DecodeString(featureEncoding)
+	if err != nil {
+		t.Fatalf("Failed to decode the feature encoding: %v", err)
+	}
+	t.Logf("Decoded feature flags: %s", decode)
+
+	featureProto := &btpb.FeatureFlags{}
+	if err = proto.Unmarshal(decode, featureProto); err != nil {
+		t.Fatalf("Failed to parse the decode: %v", err)
+	}
+	t.Logf("Parsed feature flags: %s", featureProto)
+
+	// 5. Check the featureProto to ensure every boolean field is set to true
+	enabled := true
+	fields := featureProto.ProtoReflect().Descriptor().Fields()
+	for i := 0; i < fields.Len(); i++ {
+		field := fields.Get(i)
+		value := featureProto.ProtoReflect().Get(field)
+		if field.Kind() == protoreflect.BoolKind && !value.Bool() {
+			enabled = false
+			t.Logf("field %s is not enabled", field.Name())
+		}
+	}
+	assert.True(t, enabled, "Not all the features are truly enabled")
+}
